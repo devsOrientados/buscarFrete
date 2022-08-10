dc26a88b6177ca4d9fbe9caa52b2dd3d19f276aa
alecio <santosalecio03@gmail.com>
2022-08-10 09:09:03 -0300

    finaliza a pagina do anuncio do motorista

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var motoristasRouter = require('./routes/motoristas');
+var anuncioRouter = require('./routes/anuncio');
 
 var app = express();
 
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/motoristas', motoristasRouter);
-app.use('/users', usersRouter);
+app.use('/anuncio', anuncioRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
