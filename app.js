1352131bdb154a1a8cd2c07dd7a83cacfa6d2399
Daniel Gedeão <daniel_filho_16@hotmail.com>
2022-10-13 11:01:11 -0300

    resolvendo conflitos

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var cadastroRouter = require('./routes/cadastro');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var motoristasRouter = require('./routes/motoristas');
 
 var app = express();
@@ -22,8 +22,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/motoristas', motoristasRouter);
-app.use('/anuncio', usersRouter);
-app.use('/dashboard', dashboardRouter);
 app.use('/cadastro', cadastroRouter);
 
 // catch 404 and forward to error handler
