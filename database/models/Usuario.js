9194b560370a991e5fcc1dd5d88de864a171a994
guadalupe nery <guadalupenery@gmail.com>
2022-10-21 11:26:12 +0200

    modelos criados

---
+++ database/models/Usuario.js
@@ -0,0 +1,24 @@
+module.export = (sequelize, DataTypes) =>{
+    const cols = {
+        id_usuario: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },    
+        email:{
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        senha:{
+            type: DataTypes.INTEGER,
+            allowNull: false
+        }
+    }
+    const config ={
+        tableName: 'usuarios',
+        timestamps: false
+    }
+    const Usuario = sequileze.define("Usuario", cols, config);
+    return Usuario;
+};
