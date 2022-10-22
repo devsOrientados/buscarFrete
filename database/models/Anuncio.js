c0732241d9850e63500e4413236dd0e13b8df256
guadalupe nery <guadalupenery@gmail.com>
2022-10-22 12:48:07 +0200

    modelos e view dashboard ajustados

--- database/models/Anuncio.js
+++ database/models/Anuncio.js
@@ -1,4 +1,4 @@
-module.export = (sequelize, DataTypes) =>{
+module.exports = (sequelize, DataTypes) =>{
     const cols = {
         id_anuncio: {
             type: DataTypes.INTEGER,
--- database/models/Veiculo.js
+++ database/models/Veiculo.js
@@ -1,4 +1,4 @@
-module.export = (sequelize, DataTypes) =>{
+module.exports = (sequelize, DataTypes) =>{
     const cols = {
         id_veiculo: {
             type: DataTypes.INTEGER,
