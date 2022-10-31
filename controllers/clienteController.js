e90cc7963af810f7376840d6c445e0d142d1dcb7
guadalupe nery <guadalupenery@gmail.com>
2022-10-31 15:42:41 +0100

    atualizando a rota de clientes

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -1,22 +1,24 @@
 const db = require('../database/models');
 const { validationResult } = require('express-validator');
 
-const createCadastro = {
+const cliente = {
    async Cadastro (req, res) {
       try {
          const {errors} = validationResult(req);
          if (!errors.isEmpty) {
+            console.log(errors);
             return res.render('cliente_create', {errors})
          } else {
          const {email, senha} = req.body;
          const novoUsuario = await db.Usuario.create({ email, senha});
          const {nome, sobrenome, cpf, cnh, categoria_cnh,telefone, cep, estado, cidade, bairro, logradouro, numero} =req.body;
-         const novoCliente = await db.Cliente.create({
+         await db.Cliente.create({
             nome, sobrenome, cpf, cnh, categoria_cnh,telefone, cep, estado, cidade, bairro, logradouro, numero, id_usuario:novoUsuario.id_usuario});
-         res.render('cliente_create',{errors})
+         res.redirect('/',{errors});
         }
       } catch(err){
-        res.status(400).send({errors: err.message})
+         console.log(err);
+         res.status(400).send({errors: err.message})
       }
   },
 
@@ -24,6 +26,14 @@ const createCadastro = {
    return res.render('cliente_create',{errors: []})
    },
 
+   //async Editar (req,res) => {
+   //   try {
+   //      const {errors} = validationResult(req);
+
+  //   }
+  //    catch {
+  //
+  //    }
 }
 
-module.exports = createCadastro;
+module.exports = cliente;
