51b19ba5516f3bf57991cbcb684f664f3574d44f
Guadalupe Nery <guadalupenery@gmail.com>
2022-11-03 19:47:25 +0100

    logout ok, cadastro de veiculo e servico ainda em construcao

--- controllers/servicoController.js
+++ controllers/servicoController.js
@@ -1,49 +1,52 @@
-const cidadesEstados = require('../models/Estados_Cidades.json');
+const cidadesEstados = require('../database/models/Estados_Cidades.json');
 const { validationResult } = require('express-validator');
-const db = require ('../database/models');
+const db = require('../database/models');
 
 
 const servicoController = {
-    viewBuscaServico: (req, res) =>{
-        return res.render('buscaServico',{cidadesEstados});
+    viewBuscaServico: (req, res) => {
+        return res.render('buscaServico', { cidadesEstados, usuario: req.session.usuario });
     },
-    viewServico:(req, res) => {
-        return res.render('cadastroServico',{errors: []});
+    viewServico: (req, res) => {
+        return res.render('cadastroServico', { errors: [], usuario: req.session.usuario });
 
     },
-    viewVeiculo:(req, res) => {
-        return res.render('cadastroVeiculo',{errors: []});
+    viewVeiculo: (req, res) => {
+        return res.render('cadastroVeiculo', { errors: [], usuario: req.session.usuario });
 
     },
- // ajustar as foreignKey de acordo com o modelo
-    cadastrarServico: async(req,res) => {
+
+
+    cadastrarVeiculo: async (req, res) => {
         try {
-            const {errors} = validationResult(req);
+            const { errors } = validationResult(req);
             if (!errors.isEmpty) {
-                return res.render('cadastroServico', {errors})
-             } else {
-            const novoAnuncio = {servico, autodescricao, fotoPerfil, preco} = req.body;
-            await db.Anuncio.create({servico, autodescricao, fotoPerfil, preco});
-            res.render('veiculo',{id_motorista:novoAnuncio.id_cliente})
-        }
+                return res.render('cadastroVeiculo', { errors, usuario: req.session.usuario })
+            } else {
+                const { modelo_veiculo, ano_veiculo, foto_veiculo1, foto_veiculo2, foto_veiculo3 } = req.body;
+                const cliente = await db.Cliente.findOne({ where: { id_usuario: req.session.usuario.id_usuario } })
+                await db.Veiculo.create({ modelo_veiculo, ano_veiculo, foto_veiculo1, foto_veiculo2, foto_veiculo3, id_motorista: cliente.id_cliente });
+                res.render('perfilCliente', { usuario: req.session.usuario });
+            }
         } catch (err) {
-            res.status(400).send({error: err.message});
+            res.status(400).send({ error: err.message });
         };
     },
 
- // ajustar as foreignKey de acordo com o modelo
-    cadastrarVeiculo: async(req,res) => {
+    cadastrarServico: async (req, res) => {
         try {
-            const {errors} = validationResult(req);
+            const { errors } = validationResult(req);
             if (!errors.isEmpty) {
-                return res.render('veiculo', {errors})
-             } else {
-                const {veiculo,anoVeiculo,fotoVeiculo1, fotoVeiculo2, fotoVeiculo3} = req.body;
-                await db.Veiculo.create({veiculo,anoVeiculo,fotoVeiculo1,fotoVeiculo2, fotoVeiculo3});
-                res.render('perfildomotorista');
-             }
-             } catch (err) {
-               res.status(400).send({error: err.message});
+                return res.render('cadastroServico', { errors, usuario: req.session.usuario })
+            } else {
+                const { tipo_viagem, auto_descricao, foto_motorista, preco } = req.body;
+                const cliente = await db.Cliente.findOne({ where: { id_usuario: req.session.usuario.id_usuario } });
+                const veiculo = await db.Veiculo.findOne({ where: { id_cliente: cliente.id_cliente } })
+                await db.Anuncio.create({ tipo_viagem, auto_descricao, foto_motorista, preco, id_cliente: cliente.id_cliente, id_veiculo: veiculo.id_veiculo });
+                res.render('cadastroServico', { usuario: req.session.usuario })
+            }
+        } catch (err) {
+            res.status(400).send({ error: err.message });
         };
     },
 };
