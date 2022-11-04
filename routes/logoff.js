eda9a3bf47d91ebe333f88015204f377fdcbac31
Guadalupe Nery <guadalupenery@gmail.com>
2022-11-04 11:59:01 +0100

    formularios de create(usuario, cliente, veiculo, anuncio) funcionando

--- routes/logoff.js
+++
@@ -1,8 +0,0 @@
-//var express = require('express');
-//var router = express.Router();
-//const logoffController = require('../controllers/logoffController');
-
-/* pagina de logoff*/
-//router.get('/', logoffController.viewLogin);
-
-//module.exports = router;
